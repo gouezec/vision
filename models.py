6ebb39815ecd8fd62ae18444dd37016fe9e97781
Gouezec <frederic.gouezec@free.fr>
2020-07-01 22:36:58 +0200

    Update models.py

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        
+        DROP = 0.2        
         ## TODO: Define all the layers of this CNN, the only requirements are:
         ## 1. This network takes in a square (same width and height), grayscale image as input
         ## 2. It ends with a linear layer that represents the keypoints
@@ -20,36 +20,37 @@ class Net(nn.Module):
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         # input shape
-        self.conv1 = nn.Conv2d(1, 32, 5)
-        # ouput shape: 32 x 220x220 
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        self.drop1 = nn.Dropout(0.2)
-        self.pool1 = nn.MaxPool2d(4)
-        # ouput shape: 32 x 55x55 
 
-        self.conv2 = nn.Conv2d(32, 64, 5)
-        # ouputshape: 64 x 51x51 
-        self.drop2 = nn.Dropout(0.2)
-        self.pool2 = nn.MaxPool2d(5)
-        # ouput shape: 64 x 10x10 
-        
-        self.dense3 = nn.Linear(6400, 1000)
-        self.drop3 = nn.Dropout(0.2)
-        self.dense4 = nn.Linear(1000, 136)
-        # ouput shape: 136
+        self.features = nn.Sequential(
+                            nn.Conv2d(1,32,5), #32 x 220x220 
+                            nn.ReLU(),
+                            nn.MaxPool2d(4),   #32 x 55x55
+                            nn.Dropout(DROP),
 
+                            nn.Conv2d(32,64,5),#64 x 51x51
+                            nn.ReLU(),
+                            nn.MaxPool2d(5),   #64 x 10x10
+                            nn.Dropout(DROP))
+
+        self.regression = nn.Sequential(     
+                            nn.Linear(64*10*10, 1000),
+                            nn.ReLU(),
+                            nn.Dropout(DROP),
+ #                           nn.Linear(1000, 1000),
+ #                           nn.ReLU(),
+ #                           nn.Dropout(DROP),
+                            nn.Linear(1000, 136))
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
-        x = self.drop1(self.pool1(F.relu(self.conv1(x))))
-        x = self.drop2(self.pool2(F.relu(self.conv2(x))))
+        x = self.features(x)
         x = x.view(x.size(0), -1)
-        x = self.drop3(F.relu(self.dense3(x)))
-        x = self.dense4(x)
+        x = self.regression(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
@@ -69,43 +70,42 @@ class Net2(nn.Module):
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         # input shape
         DROP = 0.2
-        self.conv1 = nn.Conv2d(1, 32, 5)
-        # ouput shape: 32 x 220x220 
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        self.drop1 = nn.Dropout(DROP)
-        self.pool1 = nn.MaxPool2d(2)
-        # ouput shape: 32 110x110 
 
-        self.conv2 = nn.Conv2d(32, 64, 3)
-        # ouputshape: 64 x 108x108
-        self.drop2 = nn.Dropout(DROP)
-        self.pool2 = nn.MaxPool2d(2)
-        # ouput shape: 64 x 54x54 
+        self.features = nn.Sequential(
+                            nn.Conv2d(1,32,5), #32 x 220x220 
+                            nn.ReLU(),
+                            nn.MaxPool2d(4),   #32 x 55x55
+                            nn.Dropout(DROP),
 
-        self.conv3 = nn.Conv2d(64, 128, 3)
-        # ouputshape: 128 x 52x52
-        self.drop3 = nn.Dropout(DROP)
-        self.pool3 = nn.MaxPool2d(4)
-        # ouput shape:  128x 13x13 
-	
-        self.dense1 = nn.Linear(21632, 1000)
-        self.dropd1 = nn.Dropout(DROP)
-        self.dense2 = nn.Linear(1000, 136)
-        # ouput shape: 136
+                            nn.Conv2d(32,64,3),#64 x 53x53
+                            nn.ReLU(),
+                            nn.MaxPool2d(3),   #64 x 18x18
+                            nn.Dropout(DROP),
 
-        
+                            nn.Conv2d(64,128,2),#128 x 17x17
+                            nn.ReLU(),
+                            nn.MaxPool2d(2),   #128 x 8x8
+                            nn.Dropout(DROP))
+
+        self.regression = nn.Sequential(     
+                            nn.Linear(128*8*8, 1000),
+                            nn.ReLU(),
+                            nn.Dropout(DROP),
+                            nn.Linear(1000, 1000),
+                            nn.ReLU(),
+                            nn.Dropout(DROP),
+                            nn.Linear(1000, 136))
+
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
-        x = self.drop1(self.pool1(F.relu(self.conv1(x))))
-        x = self.drop2(self.pool2(F.relu(self.conv2(x))))
-        x = self.drop3(self.pool3(F.relu(self.conv3(x))))
+        x = self.features(x)
         x = x.view(x.size(0), -1)
-        x = self.dropd1(F.relu(self.dense1(x)))
-        x = self.dense2(x)
+        x = self.regression(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
