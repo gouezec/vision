b25f6610a8ab8f5f447e47deeabc4639d413be9d
Gouezec <frederic.gouezec@free.fr>
2020-07-03 18:54:18 +0200

    Update models.py

--- models.py
+++ models.py
@@ -111,29 +111,38 @@ class Net3(nn.Module):
 
     def __init__(self):
         super(Net3, self).__init__()
-	self.features = nn.Sequential( 
-        			nn.Conv2d(1, 32, 5),	# 1 x 220x220
-        			nn.MaxPool2d(2, 2),	# 32 
-        		        nn.Dropout(0.1),
-        			nn.Conv2d(32, 64, 3),	#110--> 110-3+1=108
-        			nn.MaxPool2d(2, 2),     #108/2=54
-        			nn.Dropout(0.2),
-        			nn.Conv2d(64, 128, 3),	#54-->54-3+1=52
-        			nn.MaxPool2d(2, 2),	#52/2=26
-        			nn.Dropout(0.3),
-				nn.Conv2d(128, 256, 3),	#26-->26-3+1=24
-    				nn.MaxPool2d(2, 2),	#24/2=12
-        			nn.Dropout(0.4),
-        			nn.Conv2d(256,512,1),	#12-->12-1+1=12
-        			nn.MaxPool2d(2,2),	#12/2=6
-		                nn.Dropout(0.5))
-        
-	self.regression = nn.Sequential(     
-                            nn.Linear(512*6*6, 1024),
-                            nn.ReLU(),
-                            nn.Dropout(0.6),
-                            nn.Linear(1024, 136))
-		        
+        self.features = nn.Sequential( 
+                nn.Conv2d(1, 32, 5),	#1 x 220x220
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2),	    #32 x 110x110
+                nn.Dropout(0.1),
+
+                nn.Conv2d(32, 64, 3),   #64 x 108x108
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2),     #64 x 54x54
+                nn.Dropout(0.2),
+
+                nn.Conv2d(64, 128, 3),  #128 x 52x52
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2),	    #128 X 26x26
+                nn.Dropout(0.3),
+
+                nn.Conv2d(128, 256, 3), #256 x 24x24
+                nn.ReLU(),
+                nn.MaxPool2d(2, 2),	    #256 x 12x12
+                nn.Dropout(0.4),
+
+                nn.Conv2d(256,512,1),   #512 x 12x12
+                nn.ReLU(),
+                nn.MaxPool2d(2,2),	    #512 x 6x6
+                nn.Dropout(0.5))
+
+        self.regression = nn.Sequential(     
+                                nn.Linear(512*6*6, 1024),
+                                nn.ReLU(),
+                                nn.Dropout(0.6),
+                                nn.Linear(1024, 136))
+  
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
